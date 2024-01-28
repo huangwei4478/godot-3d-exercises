83e837025ebd70179e542f6dc53499a9d2ecc251
huangwei4478 <huangwei4478@users.noreply.github.com>
2024-01-28 10:26:09 +0800

    [add] RigidBody3D and Physics

--- ProjectBoost/Player.gd
+++ ProjectBoost/Player.gd
@@ -1,15 +1,12 @@
-extends Node3D
+extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:		
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		apply_central_force(basis.y * delta * 1000.0)
+	
 	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta);
+		apply_torque(Vector3(0, 0, delta * 100.0));
 		
 	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta);
-		
-	if Input.is_action_pressed("ui_up"):
-		position.y += delta;
-		
-	if Input.is_action_pressed("ui_down"):
-		position.y -= delta;
+		apply_torque(Vector3(0, 0, -delta * 100.0));
