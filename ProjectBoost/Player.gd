8d6934cebc2444769180b86cc978ec727ca1e6d3
huangwei4478 <huangwei4478@users.noreply.github.com>
2024-01-28 13:38:26 +0800

    [add] export annotations

--- ProjectBoost/Player.gd
+++ ProjectBoost/Player.gd
@@ -1,15 +1,21 @@
 extends RigidBody3D
 
+## How much vertical force to apply when moving
+@export_range(350.0, 3000.0) var thrust: float = 1000.0
+
+## How much torque force to apply when moving
+@export var torque: float = 100.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000.0)
+		apply_central_force(basis.y * delta * thrust)
 	
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0, 0, delta * 100.0));
+		apply_torque(Vector3(0, 0, delta * torque));
 		
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0, 0, -delta * 100.0));
+		apply_torque(Vector3(0, 0, -delta * torque));
 
 
 func _on_body_entered(body: Node) -> void:
