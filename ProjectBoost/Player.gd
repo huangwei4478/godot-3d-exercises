b98a847fac813487ef01c3ca7eb27233e7add176
huangwei4478 <huangwei4478@users.noreply.github.com>
2024-01-28 09:49:52 +0800

    [add] Instantiating the Player

---
+++ ProjectBoost/Player.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:		
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta);
+		
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta);
+		
+	if Input.is_action_pressed("ui_up"):
+		position.y += delta;
+		
+	if Input.is_action_pressed("ui_down"):
+		position.y -= delta;
