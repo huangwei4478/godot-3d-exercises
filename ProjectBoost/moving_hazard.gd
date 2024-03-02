445a33f52fe6c7a4c2d9762acf03f219ad4e12a2
huangwei4478 <huangwei4478@users.noreply.github.com>
2024-03-02 10:33:51 +0800

    [add] Moving Obstacles

---
+++ ProjectBoost/moving_hazard.gd
@@ -0,0 +1,18 @@
+extends AnimatableBody3D
+
+@export var destination: Vector3
+@export var duration: float = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, 'global_position', global_position + destination, duration)
+	tween.tween_property(self, 'global_position', global_position, duration)
+	tween.set_loops()
+	tween.set_trans(Tween.TRANS_SINE)
+	
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
