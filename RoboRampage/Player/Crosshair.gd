06202d5ac3eccb0971d4e47f1231093bb2cd8f68
huangwei <harw.op@qq.com>
2024-06-29 15:27:41 +0800

    [add] Prototyping Playground

--- RoboRampage/Player/Crosshair.gd
+++ RoboRampage/Player/Crosshair.gd
@@ -1,34 +1,12 @@
 @tool
 extends Control
 
-func _draw():
-	draw_circle(Vector2.ZERO, 5.0, Color.DARK_BLUE)
-	draw_circle(Vector2.ZERO, 3.0, Color.AZURE)
+func _draw() -> void:
+	draw_circle(Vector2.ZERO, 4, Color.DIM_GRAY)
+	draw_circle(Vector2.ZERO, 3, Color.WHITE)
 	
-	draw_line(
-		Vector2(16, 0),
-		Vector2(24, 0),
-		Color.AZURE,
-		3
-	)
+	draw_line(Vector2(16, 0), Vector2(24, 0), Color.WHITE, 2)
+	draw_line(Vector2(-16, 0), Vector2(-24, 0), Color.WHITE, 2)
 	
-	draw_line(
-		Vector2(-16, 0),
-		Vector2(-24, 0),
-		Color.AZURE,
-		3
-	)
-	
-	draw_line(
-		Vector2(0, 16),
-		Vector2(0, 24),
-		Color.AZURE,
-		3
-	)
-	
-	draw_line(
-		Vector2(0, -16),
-		Vector2(0, -24),
-		Color.AZURE,
-		3
-	)
+	draw_line(Vector2(0, 16), Vector2(0, 24), Color.WHITE, 2)
+	draw_line(Vector2(0, -16), Vector2(0, -24), Color.WHITE, 2)
--- RoboRampage/Player/player.gd
+++ RoboRampage/Player/player.gd
@@ -3,28 +3,26 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 
-@export var jump_height: float = 1.0		# 1 meter in the air
-@export var fall_multipler: float = 2.0		# fall faster while in the air
+@export var jump_height: float = 1.0
+@export var fall_multiplier: float = 2.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_motion := Vector2.ZERO
 
-@onready var camera_pivot = $CameraPivot
+@onready var camera_pivot: Node3D = $CameraPivot
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _physics_process(delta):
-	
-	handle_rotation()
-	
+func _physics_process(delta: float) -> void:
+	handle_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
 		if velocity.y >= 0:
 			velocity.y -= gravity * delta
 		else:
-			velocity.y -= gravity * delta * fall_multipler
+			velocity.y -= gravity * delta * fall_multiplier
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -32,8 +30,8 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -42,18 +40,18 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-
+	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			mouse_motion = -event.relative * 0.001
-		
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		
-func handle_rotation() -> void:
+func handle_camera_rotation() -> void:
 	rotate_y(mouse_motion.x)
 	camera_pivot.rotate_x(mouse_motion.y)
-	camera_pivot.rotation_degrees.x = clampf(camera_pivot.rotation_degrees.x, -90, 90)
+	camera_pivot.rotation_degrees.x = clampf(
+		camera_pivot.rotation_degrees.x, -90.0, 90.0
+	)
 	mouse_motion = Vector2.ZERO
-	
