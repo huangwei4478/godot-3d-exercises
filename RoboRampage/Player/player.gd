9b077ad241342ac1cd44d66ef7fcf86cc47ef211
huangwei <harw.op@qq.com>
2024-06-29 15:20:54 +0800

    [add] Advanced Jumping

--- RoboRampage/Player/player.gd
+++ RoboRampage/Player/player.gd
@@ -2,7 +2,9 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+@export var jump_height: float = 1.0		# 1 meter in the air
+@export var fall_multipler: float = 2.0		# fall faster while in the air
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -19,11 +21,14 @@ func _physics_process(delta):
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		if velocity.y >= 0:
+			velocity.y -= gravity * delta
+		else:
+			velocity.y -= gravity * delta * fall_multipler
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * gravity)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
