5c5bfb3da30953b881b7ba875555cca652ec6061
huangwei4478 <huangwei4478@users.noreply.github.com>
2024-09-22 18:44:09 +0800

    [add] FPS 10 Introducing Navigation

--- RoboRampage2/Player/player.gd
+++ RoboRampage2/Player/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+
+@export var jump_height := 1.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -22,7 +23,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * gravity)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
