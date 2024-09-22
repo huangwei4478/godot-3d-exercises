8db623a0f89c4fb014bc125645011759c24cbad8
huangwei4478 <huangwei4478@users.noreply.github.com>
2024-09-22 19:27:54 +0800

    [add] FPS 11 Enemy attack

--- RoboRampage2/Player/enemy.gd
+++ RoboRampage2/Player/enemy.gd
@@ -13,22 +13,18 @@ var player = null
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	navigation_agent_3d.target_position = player.global_position
+
+func _physics_process(delta: float) -> void:
 	var next_position = navigation_agent_3d.get_next_path_position()
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = global_position.direction_to(next_position)
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -36,4 +32,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-#	move_and_slide()
+	move_and_slide()
