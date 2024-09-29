594faeff054b85273defa88255fe0654ed08cb4c
huangwei4478 <huangwei4478@users.noreply.github.com>
2024-09-29 10:58:18 +0800

    [add] FPS 13 Aiming and weapons

---
+++ RoboRampage/Weapons/hitscan_weapon.gd
@@ -0,0 +1,10 @@
+extends Node3D
+@onready var cooldown_timer: Timer = $CooldownTimer
+@export var fire_rate := 14.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("fire"):
+		if cooldown_timer.is_stopped():
+			cooldown_timer.start(1.0 / fire_rate)
+			print("weapon fired!")
